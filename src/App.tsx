4c70a23d69932e0fa91d8ca5baebeda27e7049a7
Oscar Quevedo <veritly.app@gmail.com>
2026-05-31 20:39:00 -0500

    Hide Pricing section from the main page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
-import Pricing from './components/Pricing';
 import Footer from './components/Footer';
 import ConversationalForm from './components/ConversationalForm';
 
@@ -51,7 +50,6 @@ function App() {
       <main>
         <Hero onOpenSurvey={openSurvey} />
         <Features />
-        <Pricing onOpenSurvey={openSurvey} />
       </main>
       <Footer />
     </div>
